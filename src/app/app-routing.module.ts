ae41dc2022288205c4002513d4a3685d874c55fb
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-10-27 20:14:13 -0400

    Wildcard for route not matching any path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
