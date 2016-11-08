5eb1b4bdbd2271165df582cf98451f9a188dcf6a
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-11-08 17:44:51 -0500

    New Material Theme and Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     FormsModule,
     HttpModule,
     WAMRoutingModule,
-    [MaterialModule.forRoot()]
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
