f9a5f0de96c2b8e262e278a66128801cdb077ace
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-11-11 20:00:09 -0500

    Adding Login Auth Service and successfully logging a user in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { UserService } from "./shared/user.service";
+import { AuthenticationService } from "./shared/authentication.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ UserService, AuthenticationService ]
 })
 export class AppComponent {
   title = 'app works!';
