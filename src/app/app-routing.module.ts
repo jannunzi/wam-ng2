13809053b9c80bc2b5ab39ab622d49a450eb8d8d
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-11-22 01:19:52 -0500

    Reverting to last state

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login';
+import { RegisterComponent } from './register/register.component';
 import {PageNotFoundComponent} from "./page-not-found";
+import {ProfileComponent} from './profile';
+import {EditProfileComponent} from "./edit-profile/edit-profile.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  // { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'edit/:developerId', component: EditProfileComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { WAMRoutingModule } from './app-routing.module';
-import { MaterialModule } from '@angular/material';
+// import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from "angular2-materialize";
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PageNotFoundComponent
+    RegisterComponent,
+    PageNotFoundComponent,
+    PageNotFoundComponent,
+    ProfileComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     WAMRoutingModule,
-    MaterialModule.forRoot()
+    // MaterialModule.forRoot()
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
