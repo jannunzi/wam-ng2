57dc56dcab9ea0c1a1b2bf9ff9cafaa2bcf1e340
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-11-22 01:29:37 -0500

    Removing Profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,13 @@ import { MaterializeModule } from "angular2-materialize";
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    PageNotFoundComponent,
-    PageNotFoundComponent,
-    ProfileComponent,
-    EditProfileComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
