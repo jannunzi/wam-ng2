6869df86edab1c3f8383065003b820a1300251e2
Parth Ahluwalia <ahluwalia.p@husky.neu.edu>
2016-11-22 01:13:42 -0500

    Reverting to LOGIN1 merge state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { WAMRoutingModule } from './app-routing.module';
-// import { MaterialModule } from '@angular/material';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { MaterializeModule } from "angular2-materialize";
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    PageNotFoundComponent,
-    PageNotFoundComponent,
-    ProfileComponent,
-    EditProfileComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpModule,
     WAMRoutingModule,
-    // MaterialModule.forRoot()
-    MaterializeModule
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
